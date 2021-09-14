8d42f78f536a02d061e658f621b2a71bae5ff779
Adham Moamer <Adham1s2a@gmail.com>
2021-09-14 20:32:04 +0300

    Update 05_quicksort.ts

--- 04_quicksort/ts/05_quicksort.ts
+++ 04_quicksort/ts/05_quicksort.ts
@@ -11,3 +11,29 @@ function quicksort(arr: number[]): number[] {
 }
 
 console.log(quicksort([10, 5, 2, 3]));
+
+
+
+
+function QuickSort_R(arr: number[]) {
+  // the choosing of the pivot will be randomize
+  //Base Case
+  if (arr.length < 2) return arr;
+  //recrusive
+  else {
+
+    // choosing randomly the pivot from the arrays.
+      
+    let pivotindex = Math.floor(Math.random() * arr.length);
+    let pivot = arr[pivotindex];
+
+    let less = arr.filter((x) => x <= pivot).slice(0);
+    let high = arr.filter((x) => x > pivot).slice(0);
+
+    return QuickSort_R(less).concat(QuickSort_A(high));
+  }
+}
+
+console.log(QuickSort_R([3, 1, 5, 9, 7, 5, 4, 8, 34, 7, 22]));
+
+
